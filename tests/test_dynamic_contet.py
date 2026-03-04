fed8fef7ad3cfdb4a1e17715f10a45c3d3eb4eb0
Armen Cholaghian <a.cholaghian@gmail.com>
2026-03-04 13:32:46 -0500

    Better naming

--- tests/test_dynamic_contet.py
+++ tests/test_dynamic_contet.py
@@ -6,13 +6,13 @@ from config.config import config
 # Test cases for the Dynamic Content page
 
 
-# Verify the number of dynamic text blocks
+# Verify the number of dynamic text elements
 def test_dynamic_text_count(dynamic_content_page: DynamicContentPage, page : Page):
-    expected_rows = 3
-    expect(dynamic_content_page.dynamic_text()).to_have_count(expected_rows)
+    expected_amount = 3
+    expect(dynamic_content_page.dynamic_text()).to_have_count(expected_amount)
 
-# Verify the number of dynamic images
+# Verify the number of dynamic image elements
 def test_dynamic_text_count(dynamic_content_page: DynamicContentPage, page : Page):
-    expected_rows = 3
-    expect(dynamic_content_page.dynamic_image()).to_have_count(expected_rows)
+    expected_amount = 3
+    expect(dynamic_content_page.dynamic_image()).to_have_count(expected_amount)
     
